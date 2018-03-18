9a4869ebc7683470a23eb415e2bce1eb6669c4aa
zijwang <zijwang@umich.edu>
2018-03-18 15:03:46 -0400

    delete unnecessary code

--- autocompletion/HMM.py
+++
@@ -1,8 +0,0 @@
-import autocomplete
-from autocomplete import models
-
-# with open("data/parsed_email.txt") as infile:
-#     data = "\n".join(infile.readlines())
-#
-# models.train_models(data)
-autocomplete.run_server()
